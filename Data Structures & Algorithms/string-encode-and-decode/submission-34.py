3a5be9f9941e4a5e1d70d38eda2376fc5009ed9e
ArnavTheDeveloper <36607311+ArnavSarin@users.noreply.github.com>
2026-04-07 20:19:27 -0500

    Add: string-encode-and-decode - submission-34

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-34.py
@@ -0,0 +1,50 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_str_pt_uno = ''
+        encoded_str_pt_dos = '' 
+        for i in range(0,len(strs)):
+            encoded_str_pt_uno += str(len(strs[i])) + ','
+            encoded_str_pt_dos += strs[i]
+        print(encoded_str_pt_uno + '#' + encoded_str_pt_dos)
+        return encoded_str_pt_uno + '#' + encoded_str_pt_dos
+
+
+
+    def decode(self, s: str) -> List[str]:
+        ans = []
+        lengths = []
+        current_num = 0
+        starting_idx = 0
+        for i in range(0,len(s)):
+            if s[i] == '#':
+                starting_idx = i+1
+                break
+            elif s[i] == ',':
+                lengths.append(current_num)
+                current_num = 0
+            else:
+                current_num = ((current_num*10) + int(s[i]))
+        
+        print(lengths)
+
+        for i in range(0,len(lengths)):
+            if(lengths[i]==0):
+                ans.append('')
+            else:
+                print("EACH WORD")
+                print(starting_idx)
+                print(s[starting_idx])
+                print(starting_idx+lengths[i])
+                ans.append(s[starting_idx:starting_idx+lengths[i]])
+                starting_idx = starting_idx + lengths[i]
+    
+        return ans
+
+                
+
+
+
+
+
+
