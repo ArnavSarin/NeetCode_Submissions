4752c1cb4fd0bbf8c281779dc7de7da00e371c3d
ArnavTheDeveloper <36607311+ArnavSarin@users.noreply.github.com>
2026-04-14 22:09:11 -0400

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        r, l = 0,0
+        ans = ''
+        min_length = float('inf')
+
+        hash_map = Counter(t)
+
+        while r < len(s):
+            if s[r] in hash_map:
+                hash_map[s[r]] -= 1
+            while all(v <= 0 for v in hash_map.values()):
+                if r-l+1 < min_length:
+                    min_length = min(min_length,r-l+1)
+                    ans = s[l:r+1]
+                if s[l] in hash_map:
+                    hash_map[s[l]] += 1
+                l += 1
+            r += 1
+
+        return ans
