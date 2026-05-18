d627341a27e34d960b44722a779706e1bcf34ab6
ArnavTheDeveloper <36607311+ArnavSarin@users.noreply.github.com>
2026-05-18 17:48:46 -0500

    Add: course-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-4.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        egress_matrix = defaultdict(list)
+        indegrees = [0]* numCourses
+
+        for i in prerequisites:
+            egress_matrix[i[1]].append(i[0])
+            indegrees[i[0]] += 1
+
+        print(egress_matrix)
+        print(indegrees)
+
+        queue, path = deque(), []
+
+        for i in range(numCourses):
+            if indegrees[i] == 0:
+                queue.append(i)
+
+        seen = set()
+
+        while len(queue) > 0:
+            course = queue.popleft()
+
+            if course in seen:
+                break
+            
+            path.append(course)
+            seen.add(course)
+
+            for i in egress_matrix[course]:
+                indegrees[i] -= 1
+
+                if indegrees[i] == 0:
+                    queue.append(i)
+
+
+        if len(path) == numCourses:
+            return path
+        
+        return []
+
+
+            
