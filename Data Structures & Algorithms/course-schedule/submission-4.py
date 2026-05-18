52e3a4aa7e82af32108ef3429313854febc912cd
ArnavTheDeveloper <36607311+ArnavSarin@users.noreply.github.com>
2026-05-18 15:40:34 -0500

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,41 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #CREATE ADJACENCY MATRICES 
+        matrix = defaultdict(list)
+
+        for i in prerequisites:
+            matrix[i[0]].append(i[1])
+           
+        stack = []
+        seen = set()
+
+        for course in range(numCourses):
+            stack.append((course,[course]))
+            while len(stack)>0:
+                course_num, course_path = stack.pop()
+
+                if course_num in seen:
+                    break
+
+                for i in matrix[course_num]:
+                    if i in course_path:
+                        return False
+                    else:
+                        stack.append((i,course_path+[i]))
+
+                seen.add(course_num)
+
+        return True
+                                
+            
+
+
+
+
+
+
+
+
+
+
+        return True
